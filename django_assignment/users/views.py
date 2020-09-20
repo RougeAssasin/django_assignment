a9f53f426d0e251dc842503c8b36b185cd2060e0
RougeAssasin <38131121+RougeAssasin@users.noreply.github.com>
2020-09-20 20:09:53 +0530

    Update views.py

--- django_assignment/users/views.py
+++ django_assignment/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import LoginForm, SignupForm
 import requests
 from django.contrib.auth import authenticate, login, logout
@@ -9,62 +9,68 @@ from django.contrib.auth.decorators import login_required
 import secrets
 
 def login_view(request):
-	form = LoginForm()
 	if request.method == 'POST':
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			profile = Profile.objects.get(email=form.cleaned_data['email'])
+			username = profile.user	
+			password = form.cleaned_data['password']
 
-		username = Profile.objects.get(email=request.POST['email']).username	
-		password = request.POST['password']
+			user = authenticate(username = username, password = password)
+			if user is not None:
+				rsp = requests.post('http://127.0.0.1:8000/api/token/'
+					  , data={'username': username, 'password': password}).json()
 
-		user = authenticate(username = username, password = password)
-		if user is not None:
-			rsp = requests.post('http://127.0.0.1:8000/api/token/'
-				  , data={'username': username, 'password': password}).json()
+				token = rsp['access']
+				headers = {"Authorization":"Bearer " + token}
+				rsp2 = requests.get('http://127.0.0.1:8000/auth/', headers=headers)
 
-			token = rsp['access']
-			headers = {"Authorization":"Bearer " + token}
-			rsp2 = requests.get('http://127.0.0.1:8000/auth/', headers=headers)
-
-			if 'auth' in rsp2.text:
-				js = rsp2.json()
-				print(js['auth'])	
-				login(request, user)
-				return redirect(reverse('profile'))
-			
+				if 'auth' in rsp2.text:
+					js = rsp2.json()
+					print(js['auth'])	
+					login(request, user)
+					return redirect(reverse('profile'))
+				
+				else:
+					return render(request, 'login.html', {'form' : form, 'error': 'Something wrong ,try again'})
+	      	
 			else:
-				return render(request, 'login.html', {'form' : form, 'error': 'Something wrong ,try again'})
-      	
-		else:
-			return render(request, 'login.html', {'form' : form, 'error': 'Invalid Credentials'})
-		
-		# print(token)
+				return render(request, 'login.html', {'form' : form, 'error': 'Invalid Credentials'})
+
+	else:
+		form = LoginForm()
+
 	return render(request, 'login.html', { 'form': form })
 
 def signup_view(request):
-	form = SignupForm()
 	if request.method == 'POST':
-		form = SignupForm(data = request.POST)
+		form = SignupForm(request.POST, request.FILES)
 		if form.is_valid():
-			username = request.POST['first_name']
-			password = request.POST['password']
-			user = User.objects.create(username = username, password=password)
-			user.image = request.POST['image']
-			user.profile.first_name = request.POST['first_name']
-			user.profile.last_name  = request.POST['last_name']
-			user.profile.age		= request.POST['age']
-			if form['unique_id'] == "":
+			username = form.cleaned_data['first_name'] + " " + form.cleaned_data['last_name']
+			password = form.cleaned_data['password1']
+			user = User.objects.create_user(username = username, password = password)
+			user.profile.image = form.cleaned_data['image']
+			user.profile.first_name = form.cleaned_data['first_name']
+			user.profile.last_name  = form.cleaned_data['last_name']
+			user.profile.age		= form.cleaned_data['age']
+
+			if form.cleaned_data['unique_id'] == "":
 				user.profile.unique_id	= secrets.token_hex(3)
 			else:
-				user.profile.unique_id	= request.POST['unique_id']
+				user.profile.unique_id	= form.cleaned_data['unique_id']
 
-			user.profile.email		= request.POST['email']
+			user.profile.email = form.cleaned_data['email']
 			user.save()
 			form = SignupForm()
+			return redirect(reverse('login'))
+	else:
+		form = SignupForm()
 	return render(request, 'signup.html', {'form': form})
 
 @login_required
 def profile_view(request):
-	profile = Profile.objects.filter(user=request.user)
+	profile = Profile.objects.get(user=request.user)
 	if request.method == 'POST':
-		logout(user)
+		logout(request)
 		return redirect(reverse('login'))
 	return render(request, 'profile.html', {'profile': profile})
