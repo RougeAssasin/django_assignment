8aa32dd1b4d4ecb8c6be5d305304a7db6bbc4e4b
RougeAssasin <38131121+RougeAssasin@users.noreply.github.com>
2020-09-20 20:08:18 +0530

    Update forms.py

--- django_assignment/users/forms.py
+++ django_assignment/users/forms.py
@@ -1,16 +1,26 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Profile
 
-class SignupForm(forms.Form):
-    image = forms.ImageField(label='Your Picture', required = False)
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Fisrt Name'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
-    age = forms.IntegerField()
-    unique_id = forms.CharField(required = False, label='Unique Id')
+class SignupForm(UserCreationForm):
+    image = forms.ImageField(label='Your Picture')
+    first_name = forms.CharField(label='First Name', widget=forms.TextInput(attrs={'placeholder': 'Fisrt Name'}))
+    last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    age = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Age'}))
+    unique_id = forms.CharField(required = False, label='Unique Id', widget=forms.TextInput(attrs={'placeholder': 'Unique Id'}))
     email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Re-enter Password'}))
+    
+    class Meta:
+        model = User
+        exclude = ['username']
+        fields = ['image','first_name', 'last_name', 'age', 'unique_id', 'email']
 
 class LoginForm(forms.Form):
     email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email Id'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+
+        
+
+
